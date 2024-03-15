5d7c0dbd2cba6c08ba9e2fe2439e39ed08bf1475
Mayur Naik <mayurnaik364@gmail.com>
2024-03-15 19:09:10 +0530

    UPDATE: updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,25 +3,25 @@ import cors = require("cors");
 import * as dotenv from "dotenv";
 import { dbConnect } from "./database/dbConnect";
 import getTaskRouter from "./controller/getTasks";
-import createTaskRouter from "./controller/createTask"; // Changed variable name
+import createTaskRouter from "./controller/createTask";
 import updateTaskRouter from "./controller/updateTask";
-import deleteTaskRouter from "./controller/deleteTask"; // Changed variable name
+import deleteTaskRouter from "./controller/deleteTask";
 
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: process.env.PRODUCTION_URL,
+    origin: process.env.PRODUCTION_URL || "*",
     credentials: true,
     methods: ["POST", "GET", "PUT", "DELETE"],
   })
 );
 app.use(express.json());
-app.use("/api/tasks", createTaskRouter); // Updated variable name
+app.use("/api/tasks", createTaskRouter);
 app.use("/api/get-tasks", getTaskRouter);
 app.use("/api/update-tasks", updateTaskRouter);
-app.use("/api/delete-tasks", deleteTaskRouter); // Updated variable name
+app.use("/api/delete-tasks", deleteTaskRouter);
 app.use(express.urlencoded({ extended: true }));
 dbConnect();
 
